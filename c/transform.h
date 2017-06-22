27e95f250858b70d1deaf7da47b0a6d0a615a746
Jeremie <lucheng989898@gmail.com>
2017-06-22 10:53:39 +0800

    add extern "C" flag to let code work on c++ code

--- c/transform.h
+++ c/transform.h
@@ -1,9 +1,17 @@
 #ifndef TRANSFORM_HEADER
 #define TRANSFORM_HEADER
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void wgs2gcj(double wgsLat, double wgsLng, double *gcjLat, double *gcjLng);
 void gcj2wgs(double gcjLat, double gcjLng, double *wgsLat, double *wgsLng);
 void gcj2wgs_exact(double gcjLat, double gcjLng, double *wgsLat, double *wgsLng);
 double distance(double latA, double lngA, double latB, double lngB);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
